4d8a22d97ad8854372bc071ce958910914e4ba18
JethroNatividad <natividad.jet@gmail.com>
2024-01-09 23:18:18 +0800

    write tests for fahrenheit to celcius

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,19 @@
 // Process: convert temp
 // Output: The temperature in {Celsius | Fahrenheit} is {}
 
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fahrenheit_to_celcius() {
+        assert_eq!(fahrenheit_to_celcius(32.0), 0.0);
+        assert_eq!(fahrenheit_to_celcius(18.0), -7.78);
+        assert_eq!(fahrenheit_to_celcius(100.0), 37.8);
+        assert_eq!(fahrenheit_to_celcius(0.0), -17.8);
+    }
+}
 
 fn main() {
     println!("Hello, world!");
