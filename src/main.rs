e0ef1f9ac155736094ecbbe072e8d1714bd8c9d5
JethroNatividad <natividad.jet@gmail.com>
2024-01-09 23:14:58 +0800

    expand problem

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 // Program to convert celcius to fahrenheit both ways.
 
+// Inputs: Fahrenheit to celcius or celcius to fahrenheit, temperature.
+// Process: convert temp
+// Output: The temperature in {Celsius | Fahrenheit} is {}
 
 
 fn main() {
